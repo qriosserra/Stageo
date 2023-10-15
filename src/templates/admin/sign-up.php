f1ff9b1192070f5d410f4f0f2f320f3d9df89f36
louis <louis.texier@etu-montpellier.fr>
2023-10-16 01:48:09 +0200

    Admin complet et test et quelque changement fonctionnelle

--- src/templates/admin/sign-up.php
+++ src/templates/admin/sign-up.php
@@ -7,20 +7,22 @@ include __DIR__ . "/../macros/button.php";
 include __DIR__ . "/../macros/token.php";
 /**
  * @var string $token
- * @var string $login
+ * @var string $email
+ * @var string $nom
+ * @var string $prenom
  * @var array $pattern
  */
 ?>
 <main class="h-screen flex items-center justify-center gap-8 relative">
     <?=button("Accueil", "fi-rr-angle-small-left", "/Stageo", "!absolute !pl-2 top-16 left-0")?>
-    <h1 class="w-[36rem]">Nous rejoindre en tant qu'étudiant</h1>
-    <form class="flex flex-col items-center gap-4" action="<?=Action::ETUDIANT_SIGN_UP->value?>" method="post">
-        <?=input("login", "Login", "text", "fi-rr-user", "required", null, $login)?>
+    <h1 class="w-[36rem]">Crée un administrateur</h1>
+    <form class="flex flex-col items-center gap-4" action="<?=Action::ADMIN_SIGN_UP->value?>" method="post">
+        <?=input("nom", "Nom", "text", "fi-rr-user", "required", null, $nom)?>
+        <?=input("prenom", "Prenom", "text", "fi-rr-user", "required", null, $prenom)?>
+        <?=input("email", "Email", "text", "fi-rr-at", "required", null, $email)?>
         <?=input("password", "Mot de passe", "password", "fi-rr-lock", "required", $pattern["password"])?>
         <?=input("confirm", "Confirmation", "password", "fi-rr-lock", "required", $pattern["password"])?>
         <?=token($token)?>
         <input class="button-primary" type="submit" value="S'inscrire">
-        <p>Vous avez déjà un compte étudiant ? <a class="font-bold" href="<?=Action::ETUDIANT_SIGN_IN_FORM->value?>">Se connecter</a></p>
-        <p>Vous êtes une entreprise ? <a class="font-bold" href="<?=Action::ENTREPRISE_ADD_FORM->value?>">Entreprise</a></p>
     </form>
 </main>
