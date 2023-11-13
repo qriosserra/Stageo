ac3ac117dceec85b23aaa95c88aa76db98286f9c
Quentin <q.riosserra@gmail.com>
2023-11-13 15:45:39 +0100

    Integration bouton connection et deco ( louis )

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -97,18 +97,6 @@ use Stageo\Model\Object\Etudiant;
                 <a href="" class="flex items-center">
                     <img src="assets/img/logo.png" alt="logo" class="h-[1.8rem] w-[7rem] mr-3">
                 </a>
-                <div class="flex md:order-2">
-                    <a href="<?=Action::ETUDIANT_SIGN_IN_FORM->value?>" class="space-x-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <i class="fi fi-rr-sign-in-alt flex align-middle text-lg float-left"></i>
-                        <span>Se connecter</span>
-                    </a>
-                    <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
-                </div>
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white  dark:border-gray-700">
                         <li class="relative">
@@ -494,6 +482,26 @@ use Stageo\Model\Object\Etudiant;
                             </div>
                         </li>
                     </ul>
+                </div>
+                <div class="flex md:order-2">
+
+                    <?php if (is_null($user)) : ?>
+                        <a href="<?=Action::ETUDIANT_SIGN_IN_FORM->value?>" class="space-x-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <i class="fi fi-rr-sign-in-alt flex align-middle text-lg float-left"></i>
+                            <span>Se connecter</span>
+                        </a>
+                    <?php else : ?>
+                        <a href="<?=Action::SIGN_OUT->value?>" class="space-x-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <i class="fi fi-rr-sign-in-alt flex align-middle text-lg float-left"></i>
+                            <span>Se deconnecter</span>
+                        </a>
+                    <?php endif ?>
+                    <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </nav>
