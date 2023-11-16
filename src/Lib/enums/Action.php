81ac2c03122547dd5a5e6f630eb5c5895877bb3c
YvelGY <lpttgrand@gmail.com>
2023-11-16 21:54:32 +0100

    à jour avec le main

--- src/Lib/enums/Action.php
+++ src/Lib/enums/Action.php
@@ -11,7 +11,7 @@ enum Action: string
     case SIGN_OUT = "?a=signOut";
     case ETUDIANT_SIGN_IN_FORM = "?c=etudiant&a=signInForm";
     case ETUDIANT_SIGN_IN = "?c=etudiant&a=signIn";
-<<<<<<<<< Temporary merge branch 1
+
     case ENTREPRISE_SIGN_UP_STEP_1_FORM = "?c=entreprise&a=signUpStep1Form";
     case ENTREPRISE_ADD_STEP_1 = "?c=entreprise&a=signUpStep1";
     case ENTREPRISE_ADD_STEP_2_FORM = "?c=entreprise&a=signUpStep2Form";
@@ -20,18 +20,10 @@ enum Action: string
     case ENTREPRISE_ADD_STEP_3 = "?c=entreprise&a=signUpStep3";
     case ENTREPRISE_ADD_STEP_4_FORM = "?c=entreprise&a=signUpStep4Form";
     case ENTREPRISE_ADD_STEP_4 = "?c=entreprise&a=signUpStep4";
-=========
+
     case ETUDIANT_POSTULER_OFFRE = "?c=etudiant&a=postuler";
     case ETUDIANT_POSTULER_OFFRE_FORM = "?c=etudiant&a=afficherFormulairePostuler";
     case ENTREPRISE_ADD_STEP_1_FORM = "?c=entreprise&a=addStep1Form";
-    case ENTREPRISE_ADD_STEP_1 = "?c=entreprise&a=addStep1";
-    case ENTREPRISE_ADD_STEP_2_FORM = "?c=entreprise&a=addStep2Form";
-    case ENTREPRISE_ADD_STEP_2 = "?c=entreprise&a=addStep2";
-    case ENTREPRISE_ADD_STEP_3_FORM = "?c=entreprise&a=addStep3Form";
-    case ENTREPRISE_ADD_STEP_3 = "?c=entreprise&a=addStep3";
-    case ENTREPRISE_ADD_STEP_4_FORM = "?c=entreprise&a=addStep4Form";
-    case ENTREPRISE_ADD_STEP_4 = "?c=entreprise&a=addStep4";
->>>>>>>>> Temporary merge branch 2
     case ENTREPRISE_SIGN_UP_FORM = "?c=entreprise&a=signUpForm";
     case ENTREPRISE_SIGN_UP = "?c=entreprise&a=signUp";
     case ENTREPRISE_SIGN_IN_FORM = "?c=entreprise&a=signInForm";
