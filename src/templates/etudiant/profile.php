4b31597447751ed53dd3172e49a5a86a311106fa
RAM Nikhil <nikhil.ram@etu.umontpellier.fr>
2023-11-29 21:04:48 +0100

    chengement de designe

--- src/templates/etudiant/profile.php
+++ src/templates/etudiant/profile.php
@@ -8,48 +8,77 @@
             </div>
         </div>
     </div>
-    <div class="grid w-full grid-cols-1 gap-2 space-y-3 px-4 py-2 lg:grid lg:grid-cols-2 lg:space-x-3 lg:space-y-0">
-        <div class="auto-cols-auto rounded-lg border border-gray-300 bg-white hover:bg-gray-100">
-            <div class="flex justify-center bg-blue-50">
+    <div class="grid w-full grid-cols-1 gap-2 space-y-3 px-4 py-2 lg:space-x-3 lg:space-y-0">
+        <div class="auto-cols-auto rounded-lg border border-gray-300 bg-white">
+            <div class="flex justify-center border-b">
                 <span class="tracking-wide">Profil</span>
             </div>
-            <div class="grid grid-cols-1 sm:grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 1 :</span>
-                    <p>info 1</p>
+            <div class="grid grid-cols-1 xl:grid xl:grid-cols-2">
+                <div class="px-4 py-2">
+                    <span class="font-semibold tracking-wide">Nom NOM</span>
+                    <div class="flex items-center">
+                        <div class="absolute flex items-center border-r px-4 text-gray-600">
+                            <svg class="h-6 w-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4H1m3 4H1m3 4H1m3 4H1m6.071.286a3.429 3.429 0 1 1 6.858 0M4 1h12a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm9 6.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+                            </svg>
+                        </div>
+                        <input id="email2" class="flex h-10 w-full items-center rounded border border-gray-300 pl-16 text-sm font-normal text-gray-600 focus:border focus:border-indigo-700 focus:outline-none" placeholder="Nom" />
+                    </div>
                 </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 2 :</span>
-                    <p>info 2</p>
+                <div class="px-4 py-2">
+                    <span class="font-semibold tracking-wide">Prenom PRENOM</span>
+                    <div class="flex items-center">
+                        <div class="absolute flex items-center border-r px-4 text-gray-600">
+                            <svg class="h-6 w-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z" />
+                            </svg>
+                        </div>
+                        <input id="email2" class="flex h-10 w-full items-center rounded border border-gray-300 pl-16 text-sm font-normal text-gray-600 focus:border focus:border-indigo-700 focus:outline-none" placeholder="Prenom" />
+                    </div>
                 </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 3 :</span>
-                    <p>info 3</p>
+                <div class="px-4 py-2">
+                    <span class="font-semibold tracking-wide">Email EMAIL</span>
+                    <div class="flex items-center">
+                        <div class="absolute flex items-center border-r px-4 text-gray-600">
+                            <svg class="h-6 w-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z" />
+                            </svg>
+                        </div>
+                        <input id="Email" class="flex h-10 w-full items-center rounded border border-gray-300 bg-slate-100 pl-16 text-sm font-normal text-gray-600 focus:outline-none" placeholder="Email" readonly />
+                    </div>
                 </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 4 :</span>
-                    <p>info 4</p>
+                <div class="px-4 py-2">
+                    <span class="font-semibold tracking-wide">Civiliter CIVILITER</span>
+                    <div class="flex items-center">
+                        <div class="absolute flex items-center border-r px-4 text-gray-600">
+                            <svg class="h-6 w-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z" />
+                            </svg>
+                        </div>
+                        <select id="prenom" class="flex h-10 w-full items-center rounded border border-gray-300 bg-white pl-16 text-sm font-normal text-gray-600 focus:border focus:outline-none">
+                            <option value="M">HOMME</option>
+                            <option value="F">FEMME</option>
+                            <option value="F">GAY</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 5 :</span>
-                    <p>info 5</p>
+                <div class="px-4 py-2">
+                    <span class="font-semibold tracking-wide">Description DESCRIPTION</span>
+                    <div class="flex items-center">
+                        <div class="absolute flex items-center border-r px-4 text-gray-600"></div>
+                        <textarea id="bio" class="flex h-20 w-full resize-y items-center rounded border border-gray-300 p-2 text-sm font-normal text-gray-600 focus:border focus:border-indigo-700 focus:outline-none" placeholder="Écrivez votre bio ici..."></textarea>
+                    </div>
                 </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 6 :</span>
-                    <p>info 6</p>
-                </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 7 :</span>
-                    <p>info 7</p>
-                </div>
-                <div class="grid grid-cols-2 rounded px-4 py-2 text-sm text-gray-600">
-                    <span class="font-semibold tracking-wide">nom Info 8 :</span>
-                    <p>info 8</p>
+            </div>
+            <div class="border-t px-4 py-4">
+                <div class="mt-2 flex justify-center">
+                    <button class="focus:shadow-outline-green mr-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-600 focus:outline-none" id="saveButton">Sauvegarder</button>
+                    <button class="focus:shadow-outline-gray rounded-lg bg-gray-400 px-4 py-2 font-bold text-white hover:bg-gray-500 focus:outline-none" id="cancelButton">Annuler</button>
                 </div>
             </div>
         </div>
         <div class="auto-cols-auto rounded-lg border border-gray-300 bg-white hover:bg-gray-100">
-            <div class="flex justify-center bg-blue-50">
+            <div class="flex justify-center border-b">
                 <span class="tracking-wide">offres postuler</span>
             </div>
             <div>
@@ -65,14 +94,6 @@
                                     </svg>
                                 </div>
                             </button>
-                        </div>
-
-                        <div class="absolute right-0 mt-2 hidden w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 group-hover:block">
-                            <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Option 1</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Option 2</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Option 3</a>
-                            </div>
                         </div>
                     </div>
 
