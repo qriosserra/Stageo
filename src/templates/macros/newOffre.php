86ad7e05183c4744a970a6f8363b933c02f001fe
oubramk <karim.oubram@etu.umontpellier.fr>
2023-12-12 21:15:26 +0100

    improve style newOffre

--- src/templates/macros/newOffre.php
+++ src/templates/macros/newOffre.php
@@ -30,17 +30,16 @@ function newOffre(string $description = null,
     }
     //$libelle = $Categories[0]['libelle'];
     return <<<HTML
-<div class="flex rounded-lg border">
+<div class="flex rounded-lg border mb-4">
   <div class="h-24 w-24 flex-none bg-gray-200" aria-hidden="true"><!-- Placeholder for image --></div>
-  <div class="flex-grow p-4">
+  <a class="flex-grow p-4" href="$action">
     <p class="mb-2 font-bold">$description</p>
     <p class="rounded bg-blue-50 text-sm text-gray-600">$type</p>
     <p class="mb-2 mt-1 text-sm text-gray-600">$nomEntreprise</p>
     <p class="mb-2 rounded bg-blue-50 px-2 py-1 text-xs text-blue-800">$durée</p>
     <p class="text-xs">Débute le : $dateDeDebuts</p>
     <p class="mt-2 rounded bg-blue-50 px-2 py-1 text-xs text-blue-800">$libelle  </p>
-    <a href="$action" class="mt-2 inline-block text-sm text-blue-600 hover:text-blue-800">En savoir plus</a>
-  </div>
+  </a>
 </div>
 HTML;
 }
