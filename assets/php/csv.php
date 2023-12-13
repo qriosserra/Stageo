e1df943b9baffafa29b607ebba3dfb351b8bba0f
RAM Nikhil <nikhil.ram@etu.umontpellier.fr>
2023-12-13 01:49:18 +0100

    correction et finalisation de l'importation csv

--- assets/php/csv.php
+++ assets/php/csv.php
@@ -14,6 +14,7 @@ CREATE PROCEDURE importationCSV () NOT DETERMINISTIC
  DECLARE  v_IdGrat INT;
   DECLARE v_IdEnseignant INT;
  DECLARE  v_idCommune INT;
+ DECLARE v_idConvention INT;
 
  DECLARE  v_EtudiantExiste INT;
  DECLARE  v_TuteurExiste INT;
@@ -22,7 +23,11 @@ CREATE PROCEDURE importationCSV () NOT DETERMINISTIC
  DECLARE  v_DepartementUniv INT;
 DECLARE  v_idEtapExiste INT;
 DECLARE  v_convExists INT;
-
+DECLARE  v_typeStructureExiste INT;
+DECLARE  v_tailleExiste INT;
+DECLARE v_statutExiste INT;
+DECLARE v_idDistribution INT;
+DECLARE v_gratificationExiste INT;
 
 SELECT count(id_etape) INTO v_idEtapExiste
 FROM  stg_etape
@@ -54,9 +59,6 @@ WHERE login = (SELECT Numero_etudiant FROM table_temporaire WHERE id_table = 1);
 IF v_EtudiantExiste < 1 THEN 
 INSERT INTO stg_etudiant (login, nom, prenom, telephone, telephone_fixe, email_etudiant, annee, civilite, numero_voie, id_departement_universitaire, id_etape) 
 SELECT Numero_etudiant,Nom_etudiant, Prenom_etudiant,Telephone_Portable_etudiant,Telephone_Perso_etudiant, Mail_Universitaire_etudiant,annee_universitaire, Code_sexe_etudiant,NULL,Code_departement, Code_etape FROM table_temporaire WHERE id_table = 1;
-UPDATE stg_etudiant
-SET id_ufr = (SELECT code_ufr FROM table_temporaire WHERE id_table = 1)
-WHERE login = (SELECT Numero_etudiant FROM table_temporaire WHERE id_table = 1);
 ELSE
 UPDATE stg_etudiant AS e
 JOIN table_temporaire AS t ON e.login = t.Numero_etudiant
@@ -69,68 +71,75 @@ SET
     e.email_etudiant = t.Mail_Universitaire_etudiant,
     e.annee = t.annee_universitaire,
     e.civilite = t.Code_sexe_etudiant,
-    e.id_ufr = t.code_ufr,
     e.id_departement_universitaire = t.Code_departement,
     e.id_etape = t.Code_etape;
 
 
 END IF;
+
+SELECT count(id_distribution_commune) INTO v_idDistribution
+FROM stg_distribution_commune
+WHERE code_postal = (SELECT Code_postal_etudiant FROM table_temporaire where id_table = 1 LIMIT 1);
+
+IF v_idDistribution < 1 THEN 
+
+INSERT INTO stg_distribution_commune (code_postal)
+SELECT Code_postal_etudiant FROM table_temporaire where id_table = 1 LIMIT 1;
+END IF;
+
 UPDATE stg_etudiant
-SET id_distribution_commune = (SELECT id_distribution_commune FROM stg_distribution_commune WHERE code_postal = (SELECT Code_postal_etudiant FROM table_temporaire where id_table = 1))
+SET id_distribution_commune = (SELECT id_distribution_commune FROM stg_distribution_commune WHERE code_postal = (SELECT Code_postal_etudiant FROM table_temporaire where id_table = 1 LIMIT 1) LIMIT 1)
 WHERE login = (SELECT Numero_etudiant FROM table_temporaire WHERE id_table = 1);
 
 
-INSERT INTO stg_Ufr (id_ufr, libelle)
-SELECT code_ufr, Libelle_UFR FROM table_temporaire WHERE id_table = 1;
 
+SELECT count(id_unite_gratification) INTO v_gratificationExiste
+FROM stg_unite_gratification
+WHERE libelle = (SELECT Unite_Gratification FROM table_temporaire WHERE id_table = 1);
+
+IF v_gratificationExiste < 1 THEN 
 
 INSERT INTO stg_unite_gratification(libelle) 
 SELECT Unite_Gratification FROM table_temporaire WHERE id_table = 1;
-SET v_IdGrat = LAST_INSERT_ID();
+SELECT id_unite_gratification INTO v_IdGrat FROM stg_unite_gratification WHERE libelle = (SELECT Unite_Gratification FROM table_temporaire WHERE id_table = 1 LIMIT 1);
 
+ELSE
+SELECT id_unite_gratification INTO v_IdGrat FROM stg_unite_gratification WHERE libelle = (SELECT Unite_Gratification FROM table_temporaire WHERE id_table = 1 LIMIT 1);
+
+END IF;
 
 SELECT COUNT(id_enseignant) INTO v_EnseignantExiste
 FROM stg_enseignant
 WHERE prenom = (SELECT Prenom_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1)
     AND nom = (SELECT Nom_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1)
-    AND email = (SELECT Email_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1);
+    AND email = (SELECT Mail_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1);
 
 IF v_EnseignantExiste < 1 THEN 
 INSERT INTO stg_enseignant (prenom, nom, email)
-SELECT Prenom_Enseignant_referent, Nom_Enseignant_referent, Email_Enseignant_referent FROM table_temporaire WHERE id_table = 1;
-SET v_IdEnseignant = LAST_INSERT_ID();
-
-ELSE 
+SELECT Prenom_Enseignant_referent, Nom_Enseignant_referent, Mail_Enseignant_referent FROM table_temporaire WHERE id_table = 1;
+END IF;
 
 SELECT id_enseignant INTO v_IdEnseignant
 FROM stg_enseignant
 WHERE prenom = (SELECT Prenom_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1)
     AND nom = (SELECT Nom_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1)
-    AND email = (SELECT Email_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1);
+    AND email = (SELECT Mail_Enseignant_referent FROM table_temporaire WHERE id_table = 1 LIMIT 1);
 
-END IF;
+SELECT count(id_type_structure) INTO v_typeStructureExiste
+FROM stg_type_structure
+where libelle = (SELECT Type_de_Structure FROM table_temporaire WHERE id_table =1);
 
-INSERT INTO stg_suivi (date_creation, date_modification, valide, valide_pedagogiquement, avenants, details_avenants)
-SELECT STR_TO_DATE(Date_creation_conv, '%d/%m/%Y'),STR_TO_DATE(Date_modi_conv, '%d/%m/%Y') , Convention_Validee ,Con_Vali_Peda, Avenant_a_la_convention, details_avenants FROM table_temporaires WHERE id_table =1;
-
-SET v_IdSuivie = LAST_INSERT_ID();
-
-UPDATE stg_suivi
-SET id_convention = Numero_Convention
-WHERE id_suivie = v_IdSuivie;
-
+If v_typeStructureExiste < 1 THEN
 INSERT INTO stg_type_structure (libelle)
 SELECT Type_de_Structure FROM table_temporaire WHERE id_table =1;
+END IF;
 
-SET v_IdStructure = LAST_INSERT_ID();
+SELECT id_type_structure INTO v_IdStructure
+FROM stg_type_structure
+where libelle = (SELECT Type_de_Structure FROM table_temporaire WHERE id_table =1);
 
-INSERT INTO stg_taille_entreprise (libelle)
-SELECT Effectif FROM table_temporaire WHERE id_table =1;
-SET v_idTaille = LAST_INSERT_ID();
 
-INSERT INTO stg_statut_juridique (id_statut_juridique)
-SELECT Statut_Juridique FROM table_temporaire WHERE id_table =1;
-SET v_idJuridique = LAST_INSERT_ID();
+
 
 
 SELECT COUNT(id_entreprise) INTO v_EntrepriseExiste
@@ -146,12 +155,14 @@ INSERT INTO stg_entreprise (
    code_naf,
    telephone,
    fax,
-   site,
-
-   id_commune
+   site
 ) 
-SELECT Mail_Structure,siret,Adresse_Voie,Code_NAF,Telephone,Fax,SiteWeb,Eta_Accueil_Com FROM table_temporaire WHERE id_table = 1;
-SET v_idEntreprise = LAST_INSERT_ID();
+SELECT Mail_Structure,siret,Adresse_Voie,Code_NAF,Telephone,Fax,SiteWeb FROM table_temporaire WHERE id_table = 1;
+
+
+SELECT id_entreprise INTO v_idEntreprise
+FROM stg_entreprise
+WHERE siret = (SELECT siret FROM table_temporaire WHERE id_table = 1 LIMIT 1);
 
 ELSE 
 
@@ -181,8 +192,7 @@ WHERE nom = (SELECT Nom_Tuteur_Professionnel FROM table_temporaire WHERE id_tabl
 IF v_TuteurExiste < 1 THEN 
 INSERT INTO stg_tuteur (nom, prenom, email, telephone, fonction, id_entreprise)
 SELECT Nom_Tuteur_Professionnel, Prenom_Tuteur_Professionnel, Mail_Tuteur_professionnel,Telephone_Tuteur_Professionnel, Fonction_Tuteur_Professionnel,v_idEntreprise FROM table_temporaire WHERE id_table = 1;
-SET v_idTuteur = LAST_INSERT_ID();
-ELSE
+END if;
 
 SELECT id_tuteur INTO v_idTuteur
 FROM stg_tuteur
@@ -193,20 +203,53 @@ WHERE nom = (SELECT Nom_Tuteur_Professionnel FROM table_temporaire WHERE id_tabl
     AND fonction = (SELECT Fonction_Tuteur_Professionnel FROM table_temporaire WHERE id_table = 1)
     AND id_entreprise = v_idEntreprise;
 
-END IF;
 
 
-SELECT id_commune INTO v_idCommune
+SELECT count(id_distribution_commune) INTO v_idCommune
 FROM stg_distribution_commune
-where id_code_postal =(
+where code_postal =(
     SELECT Code_Postal
     FROM table_temporaire
     where id_table = 1
 );
 
+if v_idCommune < 1 THEN 
+INSERT INTO stg_distribution_commune (code_postal)
+SELECT Code_Postal FROM table_temporaire where id_table = 1 LIMIT 1;
+
+SELECT id_distribution_commune INTO v_idCommune
+FROM stg_distribution_commune
+where code_postal =(
+    SELECT Code_Postal
+    FROM table_temporaire
+    where id_table = 1
+);
+
+ELSE 
+
+SELECT id_distribution_commune INTO v_idCommune
+FROM stg_distribution_commune
+where code_postal =(
+    SELECT Code_Postal
+    FROM table_temporaire
+    where id_table = 1
+);
+
+END IF;
+
+UPDATE stg_entreprise
+SET id_distribution_commune = v_idCommune
+WHERE siret = (SELECT siret FROM table_temporaire WHERE id_table = 1 LIMIT 1);
+
+SELECT Numero_Convention INTO v_idConvention
+FROM table_temporaire 
+where id_table = 1;
+
+IF v_idConvention != Null THEN
+
 SELECT count(id_convention) INTO v_convExists
 FROM stg_convention
-WHERE id_convention = (SELECT Numero_Convention FROM table_temporaire where id_table = 1);
+WHERE id_convention = (SELECT Numero_Convention FROM table_temporaire where id_table = 1 LIMIT 1);
 
 IF v_convExists < 1 THEN 
 
@@ -237,7 +280,7 @@ INSERT INTO stg_convention (
    id_enseignant,
    id_tuteur,
    id_entreprise,
-   id_commune,
+   id_distribution_commune,
    login
 )
 SELECT Numero_Convention,
@@ -251,9 +294,9 @@ SELECT Numero_Convention,
        Detail_du_Projet,
        STR_TO_DATE(Date_Debut_Stage, '%d/%m/%Y'),
        STR_TO_DATE(Date_Fin_Stage, '%d/%m/%Y'),
-       Interruption,
-       STR_TO_DATE(date_interruption_debut, '%d/%m/%Y'),
-        STR_TO_DATE(date_interruption_fin, '%d/%m/%Y'),
+       (CASE WHEN Interruption = 'oui' THEN 1 ELSE 0 END),
+       (CASE WHEN Date_Debut_Interruption = '' THEN NULL ELSE STR_TO_DATE(Date_Debut_Interruption, '%d/%m/%Y') END),
+        (CASE WHEN Date_Fin_Interruption = '' THEN NULL ELSE STR_TO_DATE(Date_Fin_Interruption, '%d/%m/%Y') END),
        NULL,
        Nb_de_jours_de_travail,
        Nombre_d_heures_hebdomadaire,
@@ -269,7 +312,7 @@ SELECT Numero_Convention,
        v_idCommune,
        Numero_etudiant
 FROM table_temporaire 
-WHERE id_table = 1;
+WHERE id_table = 1 LIMIT 1;
 
 ELSE 
 
@@ -285,10 +328,10 @@ SET
    details = (SELECT Detail_du_Projet FROM table_temporaire WHERE id_table = 1),
    date_debut = STR_TO_DATE((SELECT Date_Debut_Stage FROM table_temporaire WHERE id_table = 1), '%d/%m/%Y'),
    date_fin = STR_TO_DATE((SELECT Date_Fin_Stage FROM table_temporaire WHERE id_table = 1), '%d/%m/%Y'),
-   interruption = (SELECT Interruption FROM table_temporaire WHERE id_table = 1),
-   date_interruption_debut = STR_TO_DATE((SELECT date_interruption_debut FROM table_temporaire WHERE id_table = 1), '%d/%m/%Y'),
-   date_interruption_fin = STR_TO_DATE((SELECT date_interruption_fin FROM table_temporaire WHERE id_table = 1), '%d/%m/%Y'),
-   heures_total = NULL, -- Remplacez cette valeur par la logique appropriée,
+   interruption = (SELECT  (CASE WHEN Interruption = 'oui' THEN 1 ELSE 0 END) FROM table_temporaire WHERE id_table = 1),
+   date_interruption_debut = (CASE WHEN Date_Debut_Interruption = '' THEN NULL ELSE STR_TO_DATE(Date_Debut_Interruption, '%d/%m/%Y') END),
+   date_interruption_fin = (CASE WHEN Date_Fin_Interruption = '' THEN NULL ELSE STR_TO_DATE(Date_Fin_Interruption, '%d/%m/%Y') END),
+   heures_total = NULL,
    jours_hebdomadaire = (SELECT Nb_de_jours_de_travail FROM table_temporaire WHERE id_table = 1),
    heures_hebdomadaire = (SELECT Nombre_d_heures_hebdomadaire FROM table_temporaire WHERE id_table = 1),
    commentaires_duree = (SELECT Commentaire_Duree_Travail FROM table_temporaire WHERE id_table = 1),
@@ -296,23 +339,25 @@ SET
    avantages_nature = (SELECT Avantages_nature FROM table_temporaire WHERE id_table = 1),
    code_elp = (SELECT Code_ELP FROM table_temporaire WHERE id_table = 1),
    numero_voie = (SELECT Adresse_Voie FROM table_temporaire WHERE id_table = 1),
-   id_unite_gratification = (SELECT v_IdGrat FROM table_temporaire WHERE id_table = 1),
-   id_enseignant = (SELECT v_IdEnseignant FROM table_temporaire WHERE id_table = 1),
-   id_tuteur = (SELECT v_idTuteur FROM table_temporaire WHERE id_table = 1),
-   id_entreprise = (SELECT v_idEntreprise FROM table_temporaire WHERE id_table = 1),
-   id_commune = (SELECT v_idCommune FROM table_temporaire WHERE id_table = 1)
+   id_unite_gratification =  v_IdGrat,
+   id_enseignant =  v_IdEnseignant ,
+   id_tuteur =  v_idTuteur,
+   id_entreprise =  v_idEntreprise ,
+   id_distribution_commune = v_idCommune
 WHERE id_convention = (SELECT Numero_Convention FROM table_temporaire WHERE id_table = 1);
 
 
 END IF;
 
+INSERT INTO stg_suivi (date_creation, date_modification, valide, valide_pedagogiquement, avenants, details_avenants,id_convention)
+SELECT STR_TO_DATE(REPLACE(Date_creation_conv, 'à', ''), '%d/%m/%Y %H:%i:%s'),STR_TO_DATE(REPLACE(Date_modi_conv, 'à', ''), '%d/%m/%Y %H:%i:%s') ,(CASE WHEN Convention_Validee = 'oui' THEN 1 ELSE 0 END)  ,(CASE WHEN Con_Vali_Peda = 'oui' THEN 1 ELSE 0 END), (CASE WHEN Avenant_a_la_convention = 'oui' THEN 1 ELSE 0 END), Details_Avenant, Numero_Convention FROM table_temporaire WHERE id_table =1;
+
+END IF;
+
 DELETE FROM table_temporaire;
-DEALLOCATE PREPARE stmt;
 end;
 //
 
 DELIMITER ;
-
-
 SQL;
 echo $query;
--- assets/php/database.php
+++ assets/php/database.php
@@ -3,3 +3,5 @@
 include "database_create.php";
 include "database_alter.php";
 include "database_insert.php";
+include "table_tamporaire.php";
+include "csv.php";
